5b9bf53c294b56849b051400ac27510a29a0443a
Luis Felipe Samaniego Bernal <luisfelipe.samaniego@bbva.com>
2021-01-27 01:57:32 -0600

    Resultado

---
+++ Practica6/Practica6-DistanciaConSensorUltrasonico.ino
@@ -0,0 +1,29 @@
+const int Trigger = 10;   //Pin digital 10 para el Trigger del sensor
+const int Echo = 9;   //Pin digital 9 para el Echo del sensor
+
+void setup() {
+  Serial.begin(9600);//iniciailzamos la comunicación
+  pinMode(Trigger, OUTPUT); //pin como salida
+  pinMode(Echo, INPUT);  //pin como entrada
+  digitalWrite(Trigger, LOW);//Inicializamos el pin con 0
+}
+
+void loop()
+{
+
+  long t; //timepo que demora en llegar el eco
+  long d; //distancia en centimetros
+
+  digitalWrite(Trigger, HIGH);
+  delayMicroseconds(10);          //Enviamos un pulso de 10us
+  digitalWrite(Trigger, LOW);
+  
+  t = pulseIn(Echo, HIGH); //obtenemos el ancho del pulso
+  d = t/59;             //escalamos el tiempo a una distancia en cm
+  
+  Serial.print("Distancia: ");
+  Serial.print(d);      //Enviamos serialmente el valor de la distancia
+  Serial.print("cm");
+  Serial.println();
+  delay(500);          //Hacemos una pausa de 500ms
+}
